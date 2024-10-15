438b17875a89d94dce9fc31e7d1d3eaf8ebef729
SE171999_NguyenLoc <nlocnguyen9@gmail.com>
2024-10-15 19:10:57 +0700

    FixUiNavBar

--- src/Redux/MiddleWare.js
+++ src/Redux/MiddleWare.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 //Call API
 const getProducts = async () => {
     const res = await axios.get("https://localhost:7085/api/products")
-    // const res = await axios.get("https://65321e684d4c2e3f333da188.mockapi.io/api/v1/categories")
     if (res.status === 200) {
         return res.data.data;
     }
@@ -12,20 +11,9 @@ const getProducts = async () => {
         return null;
     }
 }
-// const LoginAPI = async () => {
-//     const res = await axios.get("https://localhost:7085/api/auth/authentication")
-//     if (res.status === 200) {
-//         return res.data;
-//     }
-//     else {
-//         return null;
-//     }
-// }
-
 
 // Tạo Function GetData để lấy dữ liệu từ server
 function* GetData() {
-    // yield delay(1000);
     const data = yield call(getProducts)
     yield put({ type: "CHANGE_STATUS", payload: { status: true } })
     if (data) {
@@ -35,7 +23,6 @@ function* GetData() {
 
 function* mySaga() {
     yield takeLatest("GET_DATA", GetData) 
-    // yield takeLatest("GET_DATA", GetData)
 }
 
 export default mySaga;
