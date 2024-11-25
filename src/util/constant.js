768625660368fdb976e6b8fc2471cc513689e4ba
PomMort <hiuhaha789@gmail.com>
2024-11-25 21:16:11 +0700

    fix url deploy FE

--- src/util/constant.js
+++ src/util/constant.js
@@ -1,6 +1,6 @@
 export const domainFE = "http://localhost:3000";
-export const domainBE = "https://localhost:7085";
-// export const domainBE = "https://rentezapi-latest.onrender.com";
+// export const domainBE = "https://localhost:7085";
+export const domainBE = "https://rentezapi-latest.onrender.com";
 
 export const ORDER_STATUS = [
 	{
